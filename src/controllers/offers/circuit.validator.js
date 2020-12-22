406affa6590c840d24fe0d6b9d37a3648f1c05f8
Eliott Baylot <eliott.baylot@gmail.com>
2020-12-22 11:16:55 +0100

    Minor changes

---
+++ src/controllers/offers/circuit.validator.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+
+
+//export const register = {
+//  body: {
+//    firstName: Joi.string().required(),
+//    lastName: Joi.string().required(),
+//    email: Joi.string()
+//      .email()
+//      .required(),
+//    password: Joi.string().required(),
+//  },
+//};
+//
+//export const login = {
+//  body: {
+//    email: Joi.string()
+//      .email()
+//      .required(),
+//    password: Joi.string().required(),
+//  },
+//};
